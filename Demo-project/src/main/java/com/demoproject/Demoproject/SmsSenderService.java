e43653ceb0d2fb43f47b99229479f1a1dcfa5548
KERIM Abdelmouiz <abdelmouizkerim@gmail.com>
2022-02-23 21:49:28 +0100

    Add files via upload

---
+++ Demo-project/src/main/java/com/demoproject/Demoproject/SmsSenderService.java
@@ -0,0 +1,8 @@
+package com.demoproject.Demoproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsSenderService {
+	
+}
---
+++ Demo-project/src/test/java/com/demoproject/Demoproject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.Demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
